632d8e88393bcd2de221a77a96e0e1b26cf911f6
orloveste <orlovevm@yahoo.com>
2021-01-10 14:18:05 +0200

    create - repository and spring annotation

--- src/main/java/com/futureDroneV5K/droneApp/repositories/ClientRepository.java
+++ src/main/java/com/futureDroneV5K/droneApp/repositories/ClientRepository.java
@@ -2,6 +2,8 @@ package com.futureDroneV5K.droneApp.repositories;
 
 import com.futureDroneV5K.droneApp.models.Client;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface ClientRepository extends JpaRepository<Client, Long> {
 }
