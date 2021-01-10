bd5bd57f6c6a823daf87d1d6eaf803da7f757b65
orloveste <orlovevm@yahoo.com>
2021-01-10 10:51:48 +0200

    set up pom and db

---
+++ src/main/java/com/futureDroneV5K/droneApp/DroneAppApplication.java
@@ -0,0 +1,13 @@
+package com.futureDroneV5K.droneApp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DroneAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DroneAppApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/futureDroneV5K/droneApp/DroneAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.futureDroneV5K.droneApp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DroneAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
