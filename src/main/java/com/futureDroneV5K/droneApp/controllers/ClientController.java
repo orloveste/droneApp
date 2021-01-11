c299615caf5316cf5a92d1153327083850e98136
orloveste <orlovevm@yahoo.com>
2021-01-11 14:19:36 +0200

    link client page in index page menu

--- src/main/java/com/futureDroneV5K/droneApp/controllers/ClientController.java
+++ src/main/java/com/futureDroneV5K/droneApp/controllers/ClientController.java
@@ -1,7 +1,12 @@
 package com.futureDroneV5K.droneApp.controllers;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class ClientController {
+    @GetMapping("/client")
+    public String goHome() {
+        return "client";
+    }
 }
