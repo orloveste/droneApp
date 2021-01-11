004a806d969aeca60967b4d2c5c09fa5971a6cea
orloveste <orlovevm@yahoo.com>
2021-01-11 13:34:18 +0200

    test http://localhost:8080/city

---
+++ src/main/java/com/futureDroneV5K/droneApp/repositories/CityRepository.java
@@ -0,0 +1,9 @@
+package com.futureDroneV5K.droneApp.repositories;
+
+import com.futureDroneV5K.droneApp.models.City;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface CityRepository extends JpaRepository<City, Long> {
+}
