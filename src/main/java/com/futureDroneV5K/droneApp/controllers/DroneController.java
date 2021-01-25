eaa0da8b567573afc48ec5b3d663bcc19feaed18
orloveste <orlovevm@yahoo.com>
2021-01-25 16:10:08 +0200

    drone - start CRUD

--- src/main/java/com/futureDroneV5K/droneApp/controllers/DroneController.java
+++ src/main/java/com/futureDroneV5K/droneApp/controllers/DroneController.java
@@ -1,12 +1,49 @@
 package com.futureDroneV5K.droneApp.controllers;
 
+import com.futureDroneV5K.droneApp.models.Drone;
+import com.futureDroneV5K.droneApp.services.CityService;
+import com.futureDroneV5K.droneApp.services.DroneService;
+import com.futureDroneV5K.droneApp.services.RegionService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @Controller
 public class DroneController {
+    @Autowired
+    private DroneService droneService;
+    @Autowired private CityService cityService;
+    @Autowired private RegionService regionService;
+
     @GetMapping("/drones")
-    public String getDrones() {
+    public String getDrones(Model model){
+        model.addAttribute("drones", droneService.getDrone());
+
         return "drone";
+    }
+    @PostMapping("/drones/addNew")
+    public String addNew(Drone drone){
+        droneService.saveDrone(drone);
+        return "redirect:/drones";
+    }
+    @RequestMapping(value = "/drones/findById")
+    @ResponseBody
+    public Drone findById(Long id){
+        return droneService.findById(id);
+    }
+    @RequestMapping(value= "/drones/update",
+            method = {RequestMethod.PUT, RequestMethod.GET})
+    public String update(Drone drone){
+        droneService.saveDrone(drone);
+        return "redirect:/drones";
+    }
+    @RequestMapping(value = "/drones/delete",
+            method = {RequestMethod.DELETE, RequestMethod.GET})
+    public String delete(Long id){
+        droneService.delete(id);
+        return "redirect:/drones";
     }
 }
--- src/main/java/com/futureDroneV5K/droneApp/services/DroneService.java
+++ src/main/java/com/futureDroneV5K/droneApp/services/DroneService.java
@@ -1,7 +1,26 @@
 package com.futureDroneV5K.droneApp.services;
 
+import com.futureDroneV5K.droneApp.models.Drone;
+import com.futureDroneV5K.droneApp.repositories.DroneRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
 
 @Service
 public class DroneService {
+    @Autowired
+    private DroneRepository droneRepository;
+    public List<Drone> getDrone() {
+        return droneRepository.findAll();
+    }
+    public void saveDrone(Drone drone){
+        droneRepository.saveAndFlush(drone);
+    }
+    public Drone findById(Long id){
+        return droneRepository.findById(id).get();
+    }
+    public void delete(Long id) {
+        droneRepository.deleteById(id);
+    }
 }
