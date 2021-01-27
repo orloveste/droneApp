35d7e1a1f16e5d44da8c62a7dfa924ca43c050bf
orloveste <orlovevm@yahoo.com>
2021-01-27 10:36:20 +0200

    order html - do better

--- src/main/java/com/futureDroneV5K/droneApp/models/OrderCustom.java
+++ src/main/java/com/futureDroneV5K/droneApp/models/OrderCustom.java
@@ -14,7 +14,7 @@ import java.util.Date;
 @Data
 @NoArgsConstructor
 @AllArgsConstructor
-
+@JsonIdentityInfo(generator = ObjectIdGenerators.PropertyGenerator.class, property = "orderid")
 public class OrderCustom {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
