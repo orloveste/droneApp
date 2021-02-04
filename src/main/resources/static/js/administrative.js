3f3108050a924c365ae2361ab812654cfedba618
orloveste <orlovevm@yahoo.com>
2021-02-04 14:55:46 +0200

    Administrative users add avatar img - not done

---
+++ src/main/resources/static/js/administrative.js
@@ -0,0 +1,44 @@
+
+$('document').ready(function(){
+    $('table #editButton').on('click',function(event){
+        event.preventDefault();
+        var href=$(this).attr('href');
+        $.get(href, function(client, status){
+            $('#clientidEdit').val(client.clientid);
+            $('#nameEdit').val(client.name);
+            $('#addressEdit').val(client.address);
+            $('#clientcityEdit').val(client.clientcity);
+            $('#phoneEdit').val(client.phone);
+            $('#emailEdit').val(client.email);
+        });
+        $('#editModal').modal();
+    });
+    $('table #detailsButton').on('click',function(event){
+        event.preventDefault();
+        var href=$(this).attr('href');
+        $.get(href, function(client, status){
+            $('#clientidDetail').val(client.clientid);
+            $('#nameDetail').val(client.name);
+            $('#addressDetail').val(client.address);
+            $('#clientcityDetail').val(client.clientcity);
+            $('#phoneDetail').val(client.phone);
+            $('#emailDetail').val(client.email);
+        });
+        $('#detailModal').modal();
+    });
+
+    $('table #deleteButton').on('click', function(event){
+        event.preventDefault();
+        var href = $(this).attr('href');
+
+        $('#confirmDeleteButton').attr('href', href);
+        $('#deleteModal').modal();
+    });
+
+    $('.table #photoButton').on('click', function(event){
+        event.preventDefault();
+        var href = $(this).attributes('href');
+        $('#photoModal #administrativePhoto').attributes('src', href);
+        $('#photoModal').modal();
+    })
+});
